15ca91086c6173e3af3db59e185760d0a63d432c
ysheep0906 <wlq94814@naver.com>
2023-03-28 00:33:39 +0900

    add annotation

--- BOJ_10828/main.py
+++ BOJ_10828/main.py
@@ -1,13 +1,13 @@
-import sys
+import sys 
 
-class Stack:
-  arr = []
+class Stack: # Stack 클래스 선언
+  arr = [] # 데이터를 저장하는 스택을 구현하기 위해 list 사용
 
-  def push(self, num):
-    self.arr.append(num)
+  def push(self, num): 
+    self.arr.append(num) 
 
   def pop(self):
-    if(len(self.arr) == 0):
+    if(len(self.arr) == 0): # list의 길이가 0 일때
       print(-1)
     else:
       print(self.arr.pop())
@@ -25,15 +25,16 @@ class Stack:
     if(len(self.arr) == 0):
       print(-1)
     else:
-      print(self.arr[-1])
+      print(self.arr[-1]) # list의 마지막 값을 출력
 
-def run():
+def run(): 
   stack = Stack()
   N = int(input())
 
   for i in range(N):
-    input_N = sys.stdin.readline().rstrip().split()
-
+    input_N = sys.stdin.readline().split() # 시간초과를 줄이기 위해 input()말고 sys.stdin.readline()을 사용
+    # split()으로 인해 input_N의 값은 리스트 형태로 만들어지게 됨
+    # split()은 공백만 처리하는 것이 아니라 \t, \n도 처리해줌/ split(" ")은 공백만 처리
     if(input_N[0] == 'push'):
       stack.push(input_N[1])
       
